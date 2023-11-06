b82745842c38faa6563a5c665554ae604d9324a7
Andras korosi <89639857+Drewcamper@users.noreply.github.com>
2023-11-06 09:06:45 +0100

    Mock data added

--- example.js
+++ example.js
@@ -1,2 +1,118 @@
-const sayHello = () => { alert("Hello!")} 
-sayHello();
+const mockData = () => {
+  const userData = {
+    wheelData: [
+      {
+        userId: "user one",
+        userName: "John",
+        date: "December 17, 2023",
+        wheelTitle: "self-development",
+        sectorsTitles: [
+          "physical strength",
+          "mental wellbeing",
+          "private life",
+          "family and friends",
+        ],
+        layers: [
+          {
+            0: [
+              {
+                sectorTopLeft: {
+                  title: `first segment title`,
+                  description: `A detailed description for the first layers's top left segment.`,
+                  shadingLevel: 0,
+                },
+                sectorBottomLeft: {
+                  title: `second segment title`,
+                  description: `A detailed description for the second layers's top left segment.`,
+                  shadingLevel: 2,
+                },
+                sectorTopRight: {
+                  title: `third segment title`,
+                  description: `A detailed description for the third layers's top left segment.`,
+                  shadingLevel: 2,
+                },
+                sectorBottomRight: {
+                  title: `fourth segment title`,
+                  description: `A detailed description for the fourth layers's top left segment.`,
+                  shadingLevel: 1,
+                },
+              },
+            ],
+            1: [
+              {
+                sectorTopLeft: {
+                  title: `first segment title`,
+                  description: `A detailed description for the first layers's bottom left segment.`,
+                  shadingLevel: 2,
+                },
+                sectorBottomLeft: {
+                  title: `second segment title`,
+                  description: `A detailed description for the second layers's bottom left segment.`,
+                  shadingLevel: 1,
+                },
+                sectorTopRight: {
+                  title: `third segment title`,
+                  description: `A detailed description for the third layers's bottom left segment.`,
+                  shadingLevel: 1,
+                },
+                sectorBottomRight: {
+                  title: `fourth segment title`,
+                  description: `A detailed description for the fourth layers's bottom left segment.`,
+                  shadingLevel: 0,
+                },
+              },
+            ],
+            2: [
+              {
+                sectorTopLeft: {
+                  title: `first segment title`,
+                  description: `A detailed description for the first layers's bottom left segment.`,
+                  shadingLevel: 0,
+                },
+                sectorBottomLeft: {
+                  title: `second segment title`,
+                  description: `A detailed description for the second layers's bottom left segment.`,
+                  shadingLevel: 1,
+                },
+                sectorTopRight: {
+                  title: `third segment title`,
+                  description: `A detailed description for the third layers's bottom left segment.`,
+                  shadingLevel: 2,
+                },
+                sectorBottomRight: {
+                  title: `fourth segment title`,
+                  description: `A detailed description for the fourth layers's bottom left segment.`,
+                  shadingLevel: 1,
+                },
+              },
+            ],
+            3: [
+              {
+                sectorTopLeft: {
+                  title: `first segment title`,
+                  description: `A detailed description for the first layers's bottom left segment.`,
+                  shadingLevel: 0,
+                },
+                sectorBottomLeft: {
+                  title: `second segment title`,
+                  description: `A detailed description for the second layers's bottom left segment.`,
+                  shadingLevel: 2,
+                },
+                sectorTopRight: {
+                  title: `third segment title`,
+                  description: `A detailed description for the third layers's bottom left segment.`,
+                  shadingLevel: 0,
+                },
+                sectorBottomRight: {
+                  title: `fourth segment title`,
+                  description: `A detailed description for the fourth layers's bottom left segment.`,
+                  shadingLevel: 1,
+                },
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+};
