789018d744308598a472ab0dfa54c127c2b08a01
Andras korosi <89639857+Drewcamper@users.noreply.github.com>
2023-11-07 14:19:32 +0100

    discussed changes has been made

--- zone-wheel/src/mockData 17-54-26-957/MockData.js
+++ zone-wheel/src/mockData 17-54-26-957/MockData.js
@@ -1,111 +1,147 @@
 function MockData() {
-  const userData = {
+  const mockData = {
     wheelData: [
       {
         userId: "user one",
         userName: "John",
-        date: "December 17, 2023",
+        wheelId: 0, //indexing to avoid N+1 query problem in mongoDB
         wheelTitle: "self-development",
-        sectorsTitles: ["physical strength", "mental wellbeing", "private life", "family and friends"],
         layers: [
+            //key, title
           {
-            0: [
-              {
-                sectorTopLeft: {
-                  title: `first segment title`,
-                  description: `A detailed description for the first layers's top left segment.`,
-                  shadingLevel: 0,
-                },
-                sectorTopRight: {
-                  title: `third segment title`,
-                  description: `A detailed description for the third layers's top left segment.`,
-                  shadingLevel: 2,
-                },
-                sectorBottomRight: {
-                  title: `fourth segment title`,
-                  description: `A detailed description for the fourth layers's top left segment.`,
-                  shadingLevel: 1,
-                },
-                sectorBottomLeft: {
-                  title: `second segment title`,
-                  description: `A detailed description for the second layers's top left segment.`,
-                  shadingLevel: 2,
-                },
-              },
-            ],
-            1: [
-              {
-                sectorTopLeft: {
-                  title: `first segment title`,
-                  description: `A detailed description for the first layers's top left segment.`,
-                  shadingLevel: 2,
-                },
-                sectorTopRight: {
-                  title: `third segment title`,
-                  description: `A detailed description for the third layers's top left segment.`,
-                  shadingLevel: 1,
-                },
+            key: "layer-0",
+            title: "layer zero title",
+          },
+          {
+            key: "layer-1",
+            title: "layer one title",
+          },
+          {
+            key: "layer-2",
+            title: "layer two title",
+          },
+          {
+            key: "layer-3",
+            title: "layer three title",
+          },
+        ],
+        sectors: [
+          //key, title
+          {
+            key: "sector-0",
+            title: "physical strength",
+          },
+          {
+            key: "sector-1",
+            title: "mental wellbeing",
+          },
+          {
+            key: "sector-2",
+            title: "private life",
+          },
+          {
+            key: "sector-3",
+            title: "family and friends",
+          },
+        ],
 
-                sectorBottomRight: {
-                  title: `fourth segment title`,
-                  description: `A detailed description for the fourth layers's top left segment.`,
-                  shadingLevel: 0,
-                },
-                sectorBottomLeft: {
-                  title: `second segment title`,
-                  description: `A detailed description for the second layers's top left segment.`,
-                  shadingLevel: 1,
-                },
-              },
-            ],
-            2: [
-              {
-                sectorTopLeft: {
-                  title: `first segment title`,
-                  description: `A detailed description for the first layers's top left segment.`,
-                  shadingLevel: 0,
-                },
-                sectorTopRight: {
-                  title: `third segment title`,
-                  description: `A detailed description for the third layers's top left segment.`,
-                  shadingLevel: 2,
-                },
-                sectorBottomRight: {
-                  title: `fourth segment title`,
-                  description: `A detailed description for the fourth layers's top left segment.`,
-                  shadingLevel: 1,
-                },
-                sectorBottomLeft: {
-                  title: `second segment title`,
-                  description: `A detailed description for the second layers's top left segment.`,
-                  shadingLevel: 1,
-                },
-              },
-            ],
-            3: [
-              {
-                sectorTopLeft: {
-                  title: `first segment title`,
-                  description: `A detailed description for the first layers's top left segment.`,
-                  shadingLevel: 0,
-                },
-                sectorTopRight: {
-                  title: `third segment title`,
-                  description: `A detailed description for the third layers's top left segment.`,
-                  shadingLevel: 0,
-                },
-                sectorBottomRight: {
-                  title: `fourth segment title`,
-                  description: `A detailed description for the fourth layers's top left segment.`,
-                  shadingLevel: 1,
-                },
-                sectorBottomLeft: {
-                  title: `second segment title`,
-                  description: `A detailed description for the second layers's top left segment.`,
-                  shadingLevel: 2,
-                },
-              },
-            ],
+        segments: [
+          //sectorKey, layerKey, status, description
+          {
+            sectorKey: "sector-0",
+            layerKey: "layer-0",
+            status: 2,
+            description: "Description for the segment in sector 0, layer 0",
+          },
+          {
+            sectorKey: "sector-1",
+            layerKey: "layer-0",
+            status: 0,
+            description: "Description for the segment in sector 0, layer 1",
+          },
+          {
+            sectorKey: "sector-2",
+            layerKey: "layer-0",
+            status: 1,
+            description: "Description for the segment in sector 0, layer 2",
+          },
+          {
+            sectorKey: "sector-3",
+            layerKey: "layer-0",
+            status: 1,
+            description: "Description for the segment in sector 0, layer 3",
+          },
+          {
+            sectorKey: "sector-0",
+            layerKey: "layer-1",
+            status: 0,
+            description: "Description for the segment in sector 1, layer 0",
+          },
+          {
+            sectorKey: "sector-1",
+            layerKey: "layer-1",
+            status: 0,
+            description: "Description for the segment in sector 1, layer 1",
+          },
+          {
+            sectorKey: "sector-2",
+            layerKey: "layer-1",
+            status: 2,
+            description: "Description for the segment in sector 1, layer 2",
+          },
+          {
+            sectorKey: "sector-3",
+            layerKey: "layer-1",
+            status: 1,
+            description: "Description for the segment in sector 1, layer 3",
+          },
+          {
+            sectorKey: "sector-0",
+            layerKey: "layer-2",
+            status: 1,
+            description: "Description for the segment in sector 2, layer 0",
+          },
+          {
+            sectorKey: "sector-1",
+            layerKey: "layer-2",
+            status: 0,
+            description: "Description for the segment in sector 2, layer 1",
+          },
+          {
+            sectorKey: "sector-2",
+            layerKey: "layer-2",
+            status: 0,
+            description: "Description for the segment in sector 2, layer 2",
+          },
+          {
+            sectorKey: "sector-3",
+            layerKey: "layer-2",
+            status: 1,
+            description: "Description for the segment in sector 2, layer 3",
+          },
+          {
+            sectorKey: "sector-0",
+            layerKey: "layer-3",
+            status: 2,
+            description: "Description for the segment in sector 3, layer 0",
+          },
+          {
+            sectorKey: "sector-1",
+            layerKey: "layer-3",
+            status: 1,
+            description: "Description for the segment in sector 3, layer 1",
+          },
+          {
+            sectorKey: "sector-2",
+            layerKey: "layer-3",
+            status: 1,
+            description: "Description for the segment in sector 3, layer 2",
+          },
+          {
+            sectorKey: "sector-3",
+            layerKey: "layer-3",
+            status: 0,
+            description: "Description for the segment in sector 3, layer 3",
           },
         ],
       },
