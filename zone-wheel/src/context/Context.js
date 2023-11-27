d82e108f6b3c37f9e96759b858bcb18cd6d9b19a
Andras korosi <89639857+Drewcamper@users.noreply.github.com>
2023-11-27 17:28:03 +0100

    Initial version of datapoints rendering to create a wheel. Styling is only for better viewing purposes.

--- zone-wheel/src/context/Context.js
+++ zone-wheel/src/context/Context.js
@@ -6,12 +6,13 @@ export const WheelContext = createContext();
 
 export const WheelProvider = ({ children }) => {
   const [wheelState, setWheelState] = useState(() => hydrateWheelState(fetchWheelState()));
-
+  const [currentDatapointId, setCurrentDatapointId] = useState(undefined);
   return (
     <WheelContext.Provider
       value={{
         wheelState,
         setWheelState,
+        currentDatapointId, setCurrentDatapointId
       }}>
       {children}
     </WheelContext.Provider>
