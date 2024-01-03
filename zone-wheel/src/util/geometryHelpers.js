5f6ac3ebc361328f5538b7960de8e99368f005bb
Andras korosi <89639857+Drewcamper@users.noreply.github.com>
2024-01-03 17:45:47 +0100

    removed calcArcLength, because it does not belong to this branch

--- zone-wheel/src/util/geometryHelpers.js
+++ zone-wheel/src/util/geometryHelpers.js
@@ -1,17 +1,14 @@
 export const toRads = (arcAngle) => arcAngle * (Math.PI / 180);
 
-export const calcArcCoord = (x, y, innerRadius, arcAngle) => {
+export const calcArcCoord = (x, y, innerRadiusOfLevel, arcAngle) => {
   const { cos, sin } = Math;
   const radians = toRads(arcAngle - 90);
 
   return {
-    x: x + innerRadius * cos(radians),
-    y: y + innerRadius * sin(radians),
+    x: x + innerRadiusOfLevel * cos(radians),
+    y: y + innerRadiusOfLevel * sin(radians),
   };
 };
-
-  // calc the circumference of a fraction of a circle (C = 2 * π * r)
-export const calcArcLength = (aspectAngle, radius) => toRads(aspectAngle) * radius;
 
 export const calcWheelSectorAngles = (nrOfAspects, innerIndex) => {
   const aspectAngleSize = 360 / nrOfAspects;
